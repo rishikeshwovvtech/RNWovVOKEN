d6bfb4ad2c56f9a6ab421e54abe6d2d97e025161
Saurabh Kumar <saurabhkumar679@gmail.com>
2025-05-20 15:56:01 +0530

    master

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import {View, Text} from "react-native";
+import Stack from './app/src/navigation/StackNavigation';
 
 export default function App() {
   return (
-   <View>
-    <Text>Hello</Text>
-   </View>
+   <Stack />
   )
 }
